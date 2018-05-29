6f72e584dde7968903c07b3236df2d03ebba74c9
asmv <asmirno1@ucsc.edu>
2018-05-28 19:23:36 -0700

    Added data wrapper and chart title.

--- static/js/room.js
+++ static/js/room.js
@@ -1,6 +1,54 @@
+//-----ADJUSTABLE VALUES-----
+
+var COLOR_PALLATE_URL_STRING = "https://coolors.co/a23b72-67bc8b-f18f01-4f86c6-744fc6"
+
+var MAX_CHART_HEIGHT = 300;
+var MIN_CHART_HEIGHT = 100;
+var MAX_CHART_WIDTH = 700;
+var MIN_CHART_WIDTH = 100;
+
+var CHART_WIDTH_AS_PERCENTAGE_OF_WINDOW = 0.7;
+var CHART_HEIGHT_AS_PERCENTAGE_OF_WINDOW = 0.4;
+
+
+//Testing purposes only
 var JSON_TEST_STRING = '{"Question":"What movie night theme should we have?","UUID":"956e9692-5d5f-4cbc-a9a9-0d4ec7cbd4f7","Choices":[{"text":"Vampires","option_id":1,"results":{"count":6}},{"text":"Zombies","option_id":2,"results":{"count":5}},{"text":"Werewolves","option_id":3,"results":{"count":2}},{"text":"Cowboys","option_id":4,"results":{"count":14}}],"Settings":{"randomize_choice_order":true,"time_to_answer_seconds":80},"Meta":{"creation_UNIX_timestamp":1527028876,"reponse_count":27}}';
 
+//---------------------------
+
+//&&&JSON Object Wrapping&&&
+function OptionDataWrapper(json_option){
+    this.text = json_option["text"];
+    this.count = json_option["results"]["count"];
+    this.option_id = json_option["option_id"];
+}
+OptionDataWrapper.prototype.constructor = OptionDataWrapper;
+
+function ChartDataWrapper(string_data){
+    var dataobject = JSON.parse(string_data);
+    this.question = dataobject["Question"];
+    this.options = [];
+    for(var i = 0; i<dataobject["Choices"].length; i++)
+        this.options.push(new OptionDataWrapper(dataobject["Choices"][i]));
+    this.settings = dataobject["Settings"];
+    this.metadata = dataobject["Meta"];
+}
+ChartDataWrapper.prototype.constructor = ChartDataWrapper;
+ChartDataWrapper.prototype.get_option_result_count_array = function(){
+    var result_count_array = [];
+    for(var i = 0; i<this.options.length; i++){
+        result_count_array.push(this.options[i].count);
+    }
+    return result_count_array;
+};
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+//^JS Use Only, Don't Change^
+
 var PALLATE_COLORS = null
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 //++++++Helper Function++++++
 
@@ -18,8 +66,8 @@ function color_array_from_url_string(coolers_url_string){
 
 function get_bar_color_from_array_index(index){
     //https://coolors.co/a23b72-67bc8b-f18f01-4f86c6-744fc6
-    //PALLATE_COLORS = ["#A23B72", "#67BC8B", "#F18F01", "#4F86C6", "744FC6"]
     if (!PALLATE_COLORS){
+        //PALLATE_COLORS = ["#A23B72", "#67BC8B", "#F18F01", "#4F86C6", "744FC6"]
         PALLATE_COLORS = color_array_from_url_string(COLOR_PALLATE_URL_STRING);
         console.log("PALLATE_COLORS", PALLATE_COLORS)
     }
@@ -32,27 +80,13 @@ function clamp(value, max, min=0){
 
 //+++++++++++++++++++++++++++
 
-//-----ADJUSTABLE VALUES-----
-
-var COLOR_PALLATE_URL_STRING = "https://coolors.co/a23b72-67bc8b-f18f01-4f86c6-744fc6"
-
-var MAX_CHART_HEIGHT = 300;
-var MIN_CHART_HEIGHT = 100;
-var MAX_CHART_WIDTH = 700;
-var MIN_CHART_WIDTH = 100;
-
-var CHART_WIDTH_AS_PERCENTAGE_OF_WINDOW = 0.7;
-var CHART_HEIGHT_AS_PERCENTAGE_OF_WINDOW = 0.4;
-
-//---------------------------
-
 window.onload = function(){
-    console.log("hello world");
+    console.log("Page Reloaded");
     d3.select('p').text("Hello d3 selectors!");
     
     //Test code mostly following tutorials here: https://scrimba.com/g/gd3js
     
-    testdata = [4, 5, 6, 5, 3];
+//    testdata = [4, 5, 6, 5, 3];
     
 //    d3.select("#chart_test1")
 //        .selectAll("p")
@@ -99,44 +133,27 @@ window.onload = function(){
     
     draw_bargraph(JSON_TEST_STRING);
         
-//    var circle = chart_main.append("circle")
-//        .attr("cx", chart_main_width/2)
-//        .attr("cy", chart_main_height/2)
-//        .attr("r", 80)
-//        .attr("fill", "#7CE8D5");
+
     
 };
 
 
 function draw_bargraph(json_datastring_test){
     
-    function get_choice_counts(array_of_choices){
-        counts = [];
-        for(var i = 0; i<array_of_choices.length; i++){
-            counts.push(array_of_choices[i]["results"]["count"])
-        }
-        return counts
-    }
+    CDW = new ChartDataWrapper(json_datastring_test);
     
-    
-    dataobject = JSON.parse(json_datastring_test);
-    console.log(dataobject);
-    choices = dataobject["Choices"];
-    choice_counts = get_choice_counts(choices);
-
-    console.log(choice_counts)
-    console.log(choices)
+    d3.select('h2').text(CDW.question);
     
     var chart_main_width = clamp((CHART_WIDTH_AS_PERCENTAGE_OF_WINDOW*window.outerWidth), MAX_CHART_WIDTH, MIN_CHART_WIDTH);
     var chart_main_height = clamp((CHART_HEIGHT_AS_PERCENTAGE_OF_WINDOW*window.outerWidth), MAX_CHART_HEIGHT, MIN_CHART_HEIGHT);
     
     console.log("DIMENSIONS:", chart_main_width, chart_main_height)
     
-    var chart_main_bar_width = chart_main_width/choices.length;
+    var chart_main_bar_width = chart_main_width/CDW.options.length;
     var chart_main_bar_padding = Math.max(1, (0.1)*chart_main_bar_width);
     
     var chart_main_vertical_scale = d3.scaleLinear()
-        .domain([0, d3.max(choice_counts)])
+        .domain([0, d3.max(CDW.get_option_result_count_array())])
         .range([0, chart_main_height]);
     
     var chart_main = d3.select("#chart_main")
@@ -144,21 +161,28 @@ function draw_bargraph(json_datastring_test){
         .attr("height", chart_main_height);
     
     var data_bars = chart_main.selectAll("rect")
-        .data(choice_counts)
+        .data(CDW.options)
         .enter()
         .append("rect")
-        .attr("y", function(data_element){
-            return chart_main_height-chart_main_vertical_scale(data_element);
+        .attr("y", function(option){
+            return chart_main_height-chart_main_vertical_scale(option.count);
         })
         .attr("width", chart_main_bar_width-chart_main_bar_padding)
-        .attr("height", function(data_element){
-            return chart_main_vertical_scale(data_element);
+        .attr("height", function(option){
+            return chart_main_vertical_scale(option.count);
         })
-        .attr("transform", function(data_element, index){
+        .attr("transform", function(option, index){
            var translate = [chart_main_bar_width*index, 0];
            return "translate(" + translate + ")"
         })
-        .attr("fill", function(data_element, index){
+        .attr("fill", function(option, index){
               return get_bar_color_from_array_index(index);
         });
+    
+    
+//    var random_circle = chart_main.append("circle")
+//        .attr("cx", chart_main_width/2)
+//        .attr("cy", chart_main_height/2)
+//        .attr("r", 80)
+//        .attr("fill", "#7CE8D5");
 }
