8bb1d542b2b950ac295165fcf411455740545798
asmv <asmirno1@ucsc.edu>
2018-06-11 16:18:03 -0700

    Trying this pt 2.

--- controllers/api.py
+++ controllers/api.py
@@ -200,7 +200,10 @@ def reassign_poll_creator():
     except:
         print("!IMPORTANT PRINT FOLLOWS:")
         print(given_polls_dict)
-        admin_id_array = given_polls_dict.admin_id_array
+        given_polls_dict = dict(given_polls_dict)
+        print("!IMPORTANT PRINT2 FOLLOWS:")
+        print(given_polls_dict)
+        admin_id_array = given_polls_dict["admin_id_array"]
     altered_admin_ids_can_be_removed_from_cookie = []
     for admin_id in admin_id_array:
         found_poll = get_poll_by_admin_id(admin_id)
