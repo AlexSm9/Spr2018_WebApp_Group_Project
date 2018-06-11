77effb07864c86b6c6cbeed32797d976d31cf0b3
asmv <asmirno1@ucsc.edu>
2018-06-11 12:23:52 -0700

    Added api links and cookie code to profile page.

--- static/js/default_question.js
+++ static/js/default_question.js
@@ -17,12 +17,86 @@ var app = function() {
     // Enumerates an array.  Credit to Luca de Alfaro for code.
     //var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
 
-    self.get_question = function() {
-        $.getJSON(get_question_url, function(data) {
-            self.vue.question = data.question;
-            self.vue.answers = data.answers;
-        })
+//    self.get_question = function() {
+//        $.getJSON(get_question_url, function(data) {
+//            self.vue.question = data.question;
+//            self.vue.answers = data.answers;
+//        })
+//    };
+    
+    self.get_user_polls = function(){
+        
+    }
+    
+            //(%)(%)(%)(%) COOKIE FUNCTIONS (%)(%)(%)(%)
+    
+    //similar to get_cookie, but only returns the associated value from the JSON object, given a key
+    //key must be in the form of a string
+    self.from_cookie = function (key) {
+        var result = self.find_cookie();
+        if (result != "") {
+            console.log("cookie result:", result)
+            var obj = JSON.parse(result);
+            return obj[key];
+        }
+        return null;
     };
+    
+    //returns content of stored cookie (JSON string)
+    //from https://www.w3schools.com/js/js_cookies.asp
+    self.find_cookie = function () {
+        var name = "data=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    };
+    
+    //base code from https://www.w3schools.com/js/js_cookies.asp
+    //str should be a JSON string
+    self.write_string = function (str) {
+        var d = new Date();
+        //7 = number of days until cookie expires (can be changed as necessary)
+        d.setTime(d.getTime() + ((7)*24*60*60*1000));
+        expires = 'expires=' + d.toUTCString();
+        document.cookie = "data=" + str + ";" + expires + ";path=/";
+        console.log("Wrote cookie with content: " + str);
+    };
+    
+    self.add_to_cookie = function (key, value) {
+        var result = self.find_cookie();
+        if (result == "") {
+            var my_JSON = '{"' + key + '":' + value + '}';
+            console.log("Writing new cookie.");
+            console.log("key: '" + key + "', value: " + value);
+            self.write_string(my_JSON);
+        } else {
+            var obj = JSON.parse(result);
+            console.log("Adding to existing cookie: " + result);
+            console.log("key: '" + key + "', value: " + value);
+            obj[key] = value;
+            var my_JSON = JSON.stringify(obj);
+            self.write_string(my_JSON);
+        }
+    };
+    
+    self.delete_cookie_key = function(key){
+        self.add_to_cookie(key, null);
+        return;
+    }
+    
+    self.delete_cookie = function () { self.vue.is_cookie = false; };
+    
+    
+        //(%)(%)(%)(%)(%)(%)(%)(%)(%)(%)(%)(%)(%)(%)
 
     // Complete as needed.
     self.vue = new Vue({
@@ -30,15 +104,20 @@ var app = function() {
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
+            is_cookie: false,
             question: null,
             answers: []
         },
         methods: {
-            //none yet
+            get_user_polls: self.get_user_polls,
+            
+            from_cookie: self.from_cookie,
+            add_to_cookie: self.add_to_cookie,
+            delete_cookie: self.delete_cookie
         }
     });
 
-    self.get_question();
+//    self.get_question();
     $("#vue-div").show();
 
     return self;
